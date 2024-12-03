702a5a2b176421c39a2636bebb56790363c24213
lukvaz <lukas.vasquez.verdejo@gmail.com>
2024-12-03 20:04:53 -0300

    fix: env reading from populate

--- api/commands/populate.js
+++ api/commands/populate.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '../.env' });
+dotenv.config();
 
 async function clearDatabase(connection) {
     await connection.execute('DELETE FROM Inventory');
@@ -87,6 +87,13 @@ async function populateDatabase(connection) {
 }
 
 async function seedDatabase() {
+    console.log({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        port: process.env.DB_PORT,
+    })
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
